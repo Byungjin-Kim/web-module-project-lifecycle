83ae882706043d75811f286057c8985cb930cf3a
Byungjin-Kim <composerjins@gmail.com>
2023-02-03 10:58:38 -0600

    Added in all dom.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,6 +4,23 @@ const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <ul>
+          <li>Walking with my Dog</li>
+          <li>Taking care of my kids</li>
+          <li>Studying React!</li>
+        </ul>
+
+        <form>
+          <input />
+          <button>Submit</button>
+        </form>
+        <button>Hide Completed</button>
+      </div>
+
+
+
+    )
   }
 }
